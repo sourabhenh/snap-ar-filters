55e3cfa96d11a041bdcddef576828beeda48441f
Sourabh <sourabh@enhmedia.com>
2026-01-29 17:45:24 +0400

    Analytic Addedd

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,26 @@ import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import Home from "./pages/Home";
 import France from "./pages/France";
 import Argentina from "./pages/Argentina";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react"; // Check import path, usually 'react' for Vite apps
 
 function App() {
   return (
-      
-    <BrowserRouter>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/france">France</NavLink>
-        <NavLink to="/argentina">Argentina</NavLink>
-      </nav>
+    <>
+      <BrowserRouter>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/france">France</NavLink>
+          <NavLink to="/argentina">Argentina</NavLink>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/france" element={<France />} />
-        <Route path="/argentina" element={<Argentina />} />
-      </Routes>
-    </BrowserRouter>
-       <Analytics />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/france" element={<France />} />
+          <Route path="/argentina" element={<Argentina />} />
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
 
