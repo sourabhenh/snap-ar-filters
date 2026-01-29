aaa0dd986f1b0d5165c15158d275bf365d46a875
Sourabh <sourabh@enhmedia.com>
2026-01-29 17:42:30 +0400

    Analytic Addedd

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import Home from "./pages/Home";
 import France from "./pages/France";
 import Argentina from "./pages/Argentina";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
+      
     <BrowserRouter>
       <nav>
         <NavLink to="/">Home</NavLink>
@@ -19,6 +21,7 @@ function App() {
         <Route path="/argentina" element={<Argentina />} />
       </Routes>
     </BrowserRouter>
+       <Analytics />
   );
 }
 
